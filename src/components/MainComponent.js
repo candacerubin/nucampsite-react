38c157ee74fc1a167d6b6a06c3ffd9bc3735f5b0
Candace Rubin <candacerubin0@gmail.com>
2020-10-24 11:29:44 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -33,6 +33,15 @@ class Main extends Component {
 			);
 		};
 
+		const CampsiteWithId = ({ match }) => {
+			return (
+				<CampsiteInfo
+					campsite={this.state.campsites.filter((campsite) => campsite.id === +match.params.campsiteId)[0]}
+					comments={this.state.comments.filter((comment) => comment.campsiteId === +match.params.campsiteId)}
+				/>
+			);
+		};
+
 		return (
 			<div>
 				<Header />
@@ -40,6 +49,7 @@ class Main extends Component {
 					<Route path="/home" component={HomePage} />
 					<Route exact path="/contactus" component={Contact} />
 					<Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+					<Route path="/directory/:campsiteId" component={CampsiteWithId} />
 					<Redirect to="/home" />
 				</Switch>
 				<Footer />
