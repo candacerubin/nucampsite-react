e69f25bc5b541df47a33652d49f819d1880a9e36
candacerubin <candacerubin0@gmail.com>
2020-11-06 19:36:05 -0800

    React Redux Form Validation

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,102 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, Label, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
+class CommentForm extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			author: '',
+			isModalOpen: false,
+			touched: {
+				author: false,
+			},
+		};
+
+		this.toggleModal = this.toggleModal.bind(this);
+	}
+
+	toggleModal() {
+		this.setState({
+			isModalOpen: !this.state.isModalOpen,
+		});
+	}
+
+	handleSubmit(values) {
+		console.log('Current state is: ' + JSON.stringify(values));
+		alert('Current state is: ' + JSON.stringify(values));
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<Button type='submit' outline onClick={this.toggleModal}>
+					<i className='fa fa-pencil fa-lg'> Submit Comment</i>
+				</Button>
+
+				<Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+					<ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+					<ModalBody>
+						<LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+							<div className='form-group'>
+								<Label htmlFor='rating'>Rating</Label>
+								<Control.select model='.rating' name='rating' className='form-control'>
+									<option>1</option>
+									<option>2</option>
+									<option>3</option>
+									<option>4</option>
+									<option>5</option>
+								</Control.select>
+							</div>
+
+							<div className='form-group'>
+								<Label htmlFor='author'>Your Name</Label>
+								<Control.text
+									model='.author'
+									id='author'
+									name='author'
+									placeholder='Your Name'
+									className='form-control'
+									validators={{
+										required,
+										minLength: minLength(2),
+										maxLength: maxLength(15),
+									}}
+								/>
+								<Errors
+									className='text-danger'
+									model='.author'
+									show='touched'
+									component='div'
+									messages={{
+										required: 'Required',
+										minLength: 'Must be at least 2 characters',
+										maxLength: 'Must be 15 characters or less',
+									}}
+								/>
+							</div>
+
+							<div className='form-group'>
+								<Label htmlFor='comment'>Comment</Label>
+								<Control.textarea model='.text' id='comment' name='comment' className='form-control' />
+							</div>
+
+							<Button type='submit' value='submit' color='primary'>
+								Submit
+							</Button>
+						</LocalForm>
+					</ModalBody>
+				</Modal>
+			</React.Fragment>
+		);
+	}
+}
 
 function RenderSelectedCampsite({ campsite }) {
 	return (
@@ -30,6 +126,9 @@ function RenderComments({ comments }) {
 						</div>
 					);
 				})}
+				<span>
+					<CommentForm></CommentForm>
+				</span>
 			</div>
 		);
 	} else {
@@ -40,12 +139,12 @@ function RenderComments({ comments }) {
 function CampsiteInfo(props) {
 	if (props.campsite) {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col">
+			<div className='container'>
+				<div className='row'>
+					<div className='col'>
 						<Breadcrumb>
 							<BreadcrumbItem>
-								<Link to="/directory">Directory</Link>
+								<Link to='/directory'>Directory</Link>
 							</BreadcrumbItem>
 							<BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
 						</Breadcrumb>
@@ -53,7 +152,7 @@ function CampsiteInfo(props) {
 						<hr />
 					</div>
 				</div>
-				<div className="row">
+				<div className='row'>
 					<RenderSelectedCampsite campsite={props.campsite} />
 					<RenderComments comments={props.comments} />
 				</div>
